1b3c6dcc15709c6c15a7e1fe74c8b84ad3a7077b
ucarkadir <ucarkadir@outlook.com>
2019-04-27 18:06:27 +0300

    sqllite-crud-windows-form

---
+++ 13_Build_a_Desktop_Database_Application/168_UserInterface_Design/backend.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book ( id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? or author=? or year=? or isbn=?",(title, author, year, isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn,id,))
+    conn.commit()
+    conn.close()
+
+connect()
+
+# insert("kitap1", "yazar1", 1, 1111)
+
+# insert("kitap2", "yazar2",2,2222)
+print(search(title="kitap1"))
+# insert("kitap3", "yazar3",3,3333)
+# delete(3)
+# update(id=2, title="kitaps",author="yazars",year=3,isbn=3333)
+# insert("kitap4", "yazar4",4,4444)
+print(view())
