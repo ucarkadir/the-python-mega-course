c6a968c7a2c741fdb876dc729c98d9ae58e75bf9
kadir.ucar <kadir.ucar@dbhsoft.com>
2019-04-30 13:40:33 +0300

    delete acc

--- 14_object_oriented_programming/176_creating_a_bank_account_object/acc (2).py
+++
@@ -1,8 +0,0 @@
-class Account:
-
-    def __init__(self, filepath):
-        with open(filepath, 'r') as file:
-            self.balance=int(file.read())
-
-accoun=Account("balance.txt")
-print(account)
